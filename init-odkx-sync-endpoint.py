6050b81014fc03fbfc43a5d0ef210e19b048f117
sanyud24 <sanyu.daver.civ19@itbhu.ac.in>
2022-04-04 22:57:56 +0530

    redid the whole script for manual certificates
    fix typo

--- init-odkx-sync-endpoint.py
+++ init-odkx-sync-endpoint.py
@@ -11,13 +11,14 @@ import os
 import re
 from tempfile import mkstemp
 from shutil import move, copymode
-from os import fdopen, remove
+from os import fdopen, remove, path
+from xml import dom
 
 def run_interactive_config():
     env_file_location = os.path.join(os.path.dirname(__file__), "config", "https.env")
 
     try:
-        domain, email = parse_env_file(env_file_location)
+        env = parse_env_file(env_file_location)
         print("Found configuration at {}".format(env_file_location))
     except OSError:
         print("No default https configuration file found at expected path {}. This prevents automatically renewing certs!".format(env_file_location))
@@ -30,10 +31,10 @@ def run_interactive_config():
     time.sleep(1)
     print("")
     print("Please input the domain name you will use for this installation. A valid domain name is required for HTTPS without distributing custom certificates.")
-    input_domain = input("domain [({})]:".format(domain))
+    input_domain = input("domain [({})]:".format(env["HTTPS_DOMAIN"]))
 
     if input_domain != "":
-        domain = input_domain
+        env["HTTPS_DOMAIN"] = input_domain
 
     print("")
     use_custom_password = input("Do you want to use a custom LDAP administration password (y/N)?")
@@ -48,19 +49,19 @@ def run_interactive_config():
 
     while True:
         print("Would you like to enforce HTTPS? We recommend yes.")
-        enforce_https = input("enforce https [(Y)/n]:")
+        enforce_https = input("enforce https [(Y)/n]:").lower().strip()
         if enforce_https == "":
             enforce_https = "y"
-            enforce_https = enforce_https.lower().strip()[0]
+            enforce_https = enforce_https[0]
         if enforce_https in ["y", "n"]:
             break
 
     if enforce_https == "n":
         print("Would you like to run an INSECURE and DANGEROUS server that will share your users's information if exposed to the Internet?")
-        insecure = input("run insecure [y/(N)]:")
+        insecure = input("run insecure [y/(N)]:").lower().strip()
         if insecure == "":
             insecure = "n"
-        if insecure.lower().strip()[0] != "y":
+        if insecure[0] != "y":
             raise RuntimeError("HTTPS is currently required to run a secure public server. Please restart and select to enforce HTTPS")
 
     enforce_https = enforce_https == "y"
@@ -68,29 +69,28 @@ def run_interactive_config():
     print("Enforcing https:", enforce_https)
     if enforce_https:
         print("Please provide an admin email for security updates with HTTPS registration")
-        input_email = input("admin email [({})]:".format(email))
+        input_email = input("admin email [({})]:".format(env["HTTPS_ADMIN_EMAIL"]))
 
         if input_email != "":
-            email = input_email
+            env["HTTPS_ADMIN_EMAIL"] = input_email
 
         print("The system will now attempt to setup an HTTPS certificate for this server.")
         print("For this to work you must have already have purchased/acquired a domain name (or subdomain) and setup a DNS A or AAAA record to point at this server's IP address.")
         print("If you have not done this yet, please do it now...")
         time.sleep(1)
-        proceed = input("Domain is ready to proceed with certificate acquisition? [(Y)/n]")
+        proceed = input("Domain is ready to proceed with certificate acquisition? [(Y)/n]").strip().lower()
         if proceed == "":
             proceed = "y"
-        if proceed.strip().lower()[0] != "y":
+        if proceed[0] != "y":
             print("Re-run this script once the domain is ready!")
             exit(1)
-        
-        print("Do you wish to supply you own SSL certificate .")
-        manual_certificate = input(["y/(N)"])
+
+        print("Do you wish to supply your own SSL certificate? If not, the script will use certbot (please make sure it is already installed).")
+        manual_certificate = input(["y/(N)"]).strip().lower()
 
         if manual_certificate == "" :
             manual_certificate = "n"
-        if manual_certificate.strip().lower()[0] != "y":
-
+        if manual_certificate[0] != "y":
             os.system("sudo certbot certonly --standalone \
             --email {} \
             -d {} \
@@ -98,15 +98,30 @@ def run_interactive_config():
             --agree-tos \
             --cert-name bootstrap \
             --keep-until-expiring \
-            --non-interactive".format(email, domain))
-
-           
-        elif manual_certificate.strip().lower()[0] == "y":
-            os.system("sudo systemctl restart nginx")
+            --non-interactive".format(env["HTTPS_ADMIN_EMAIL"], env["HTTPS_DOMAIN"]))
+        else:
+            cert_fullchain_path = env.get("CERT_FULLCHAIN_PATH")
+            cert_privkey_path = env.get("CERT_PRIVKEY_PATH")
+            print('Please enter path to fullchain .pem/.crt file')
+            cert_fullchain_path = input("fullchain file [({})]:".format(cert_fullchain_path)).strip() or cert_fullchain_path
+            print('Please enter path to private key .pem file')
+            cert_privkey_path = input("private key file [({})]:".format(cert_privkey_path)).strip() or cert_privkey_path
+            if not cert_fullchain_path or not cert_privkey_path:
+                print('Input not provided, re-run this script with correct inputs.')
+                exit(1)
+            # Compute absolute paths from relative path inputs
+            cert_fullchain_path = path.abspath(cert_fullchain_path)
+            cert_privkey_path = path.abspath(cert_privkey_path)
+            if not path.exists(cert_fullchain_path) or not path.exists(cert_privkey_path):
+                print('File at the given path do not exists, re-run this script with correct inputs.')
+                exit(1)
+            env['CERT_FULLCHAIN_PATH'] = cert_fullchain_path
+            env['CERT_PRIVKEY_PATH'] = cert_privkey_path
 
         print("Attempting to save updated https configuration")
-        write_to_env_file(env_file_location, domain, email)    
-    return enforce_https
+        write_to_env_file(env_file_location, env)
+
+    return (enforce_https, env)
 
 
 def replaceInFile(file_path, pattern, subst):
@@ -119,35 +134,27 @@ def replaceInFile(file_path, pattern, subst):
     remove(file_path)
     move(abs_path, file_path)
 
-def write_to_env_file(filepath, domain_name, email):
+
+def write_to_env_file(filepath, env: dict):
     """A janky in-memory file write.
 
     This is not atomic and would use lots of ram for large files.
     """
-    file_lines = []
-    with open(filepath, mode="r") as f:
-        for line in f:
-            file_lines.append(line)
-
     with open(filepath, mode="w") as f:
-        for line in file_lines:
-            if line.startswith("HTTPS_DOMAIN="):
-                line = "HTTPS_DOMAIN={}\n".format(domain_name)
-            if line.startswith("HTTPS_ADMIN_EMAIL="):
-                line = "HTTPS_ADMIN_EMAIL={}\n".format(email)
-            f.write(line)
+        for (key, val) in env.items():
+            f.write("{}={}\n".format(key, val))
 
 
 def parse_env_file(filepath):
-    domain = None
-    email = None
+    env = {}
     with open(filepath) as f:
         for line in f:
-            if line.startswith("HTTPS_DOMAIN="):
-                domain=line[13:].strip()
-            if line.startswith("HTTPS_ADMIN_EMAIL="):
-                email=line[18:].strip()
-    return (domain,email)
+            try:
+                key, val = line.split('=')
+            except Exception:
+                continue
+            env[key] = val.strip()
+    return env
 
 
 def run_docker_builds():
@@ -163,15 +170,20 @@ def run_sync_endpoint_build():
                mvn -pl org.opendatakit:sync-endpoint-war,org.opendatakit:sync-endpoint-docker-swarm,org.opendatakit:sync-endpoint-common-dependencies clean install -DskipTests")
 
 
-def deploy_stack(use_https):
+def deploy_stack(use_https, env):
     if use_https:
-        os.system("docker stack deploy -c docker-compose.yml -c docker-compose-https.yml syncldap")
+        is_certbot = 'CERT_FULLCHAIN_PATH' not in env
+        config = 'docker-compose-https-certbot.yml' if is_certbot else 'docker-compose-https.yml'
+        envstring = ""
+        if not is_certbot:
+            envstring = "env CERT_FULLCHAIN_PATH={} CERT_PRIVKEY_PATH={}".format(env["CERT_FULLCHAIN_PATH"], env["CERT_PRIVKEY_PATH"])
+        os.system("{} docker stack deploy -c docker-compose.yml -c {} syncldap".format(envstring, config))
     else:
         os.system("docker stack deploy -c docker-compose.yml syncldap")
 
 
 if __name__ == "__main__":
-    https = run_interactive_config()
+    https, env = run_interactive_config()
     run_docker_builds()
     run_sync_endpoint_build()
-    deploy_stack(https)
+    deploy_stack(https, env)
