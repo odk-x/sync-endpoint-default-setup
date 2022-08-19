8d0d6b40d33c62919c24471954ffd82a8491dc3c
jesse5555 <natukunda55455@gmail.com>
2022-08-20 00:21:56 +0300

    add ability to support Internationalisation
    This commit uses python's locale module to get the default language set in your system and the default gettext module to load the messages basing on the provided locale.  In case there is no default language set, English is taken as default and the user is notified about this.
    
    solved french not working
    
    solved mypy issue
    
    remove pytest cache

--- init-odkx-sync-endpoint.py
+++ init-odkx-sync-endpoint.py
@@ -11,33 +11,57 @@ import os
 import re
 import typer
 import json
+import locale
+import gettext
 from tempfile import mkstemp
 from shutil import move, copymode
 from os import fdopen, remove, path
-from typing import Dict, Union
+from typing import Dict, Union, Any
 from xml import dom
 
 app = typer.Typer(add_completion=False)
+
+_: Any
+
+def setup_locale() -> str:
+# setup locale
+    defLocale = locale.getdefaultlocale()
+    languageCounty = defLocale[0]   # en_UK
+    if not languageCounty:
+        # if language is not one of the supported languages, we use english
+        typer.echo("Oups! No translations found for your language... Using english.")
+        typer.echo("Please send your translations for improvements.")
+        typer.echo("")
+        language = 'en'
+    else:
+        language = languageCounty[:2]  # en
+    return language
+
+def setup_translator(language_code):
+    localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
+    translate = gettext.translation('odk-x-sync-endpoint-init', localedir, languages=[language_code], fallback=True)
+    translate.install()
+    _ = translate.gettext
 
 def run_interactive_config():
     env_file_location = os.path.join(os.path.dirname(__file__), "config", "https.env")
 
     try:
         env = parse_env_file(env_file_location)
-        typer.echo(f"Found configuration at {env_file_location}")
+        typer.echo(_("Found configuration at {env_file_location}").format(env_file_location=env_file_location))
     except OSError:
-        typer.echo(f"No default https configuration file found at expected path {env_file_location}. This prevents automatically renewing certs!")
-        typer.echo("Please check your paths and file permissions, and make sure your config repo is up to date.")
+        typer.echo(_("No default https configuration file found at expected path {env_file_location}. This prevents automatically renewing certs!").format(env_file_location=env_file_location))
+        typer.echo(_("Please check your paths and file permissions, and make sure your config repo is up to date."))
         raise typer.Exit()
 
-    typer.echo("Welcome to the ODK-X sync endpoint installation!")
-    typer.echo("This script will guide you through setting up your installation")
-    typer.echo("We'll need some information from you to get started though...")
+    typer.echo(_("Welcome to the ODK-X sync endpoint installation!"))
+    typer.echo(_("This script will guide you through setting up your installation"))
+    typer.echo(_("We'll need some information from you to get started though..."))
     time.sleep(1)
     typer.echo("")
 
     if is_cache_present() and is_complete_cache():
-        allow_cache = typer.confirm("Do you wish to use cached configuration?", default=True)
+        allow_cache = typer.confirm(_("Do you wish to use cached configuratilon?"), default=True)
         typer.echo("")
         if allow_cache:
             cached_data = load_progress()
@@ -57,73 +81,73 @@ def run_interactive_config():
 
     return (enforce_https, env)
 
-def run_cache_setup(envParam) -> bool:
+def run_cache_setup(envParam: Dict[str, str]) -> bool:
 
-    env:Dict[str, str] = envParam
-    typer.echo("Please input the domain name you will use for this installation. A valid domain name is required for HTTPS without distributing custom certificates.")
-    input_domain: str = typer.prompt(f"domain [({env['HTTPS_DOMAIN']})]", default=env['HTTPS_DOMAIN'], show_default=False)
+    typer.echo(_("Please input the domain name you will use for this installation. A valid domain name is required for HTTPS without distributing custom certificates."))
+    input_domain: str = typer.prompt(_("domain [({envParam_https_domain})]").format(envParam_https_domain=envParam['HTTPS_DOMAIN']), default=envParam['HTTPS_DOMAIN'], show_default=False)
 
     if input_domain != "":
-        env['HTTPS_DOMAIN'] = input_domain
+        envParam['HTTPS_DOMAIN'] = input_domain
         save_progress('env', {'HTTPS_DOMAIN': input_domain})
 
 
     typer.echo("")
-    use_custom_password = typer.confirm("Do you want to use a custom LDAP administration password?")
+    use_custom_password = typer.confirm(_("Do you want to use a custom LDAP administration password?"))
     if use_custom_password:
         typer.echo("")
-        typer.echo("Please input the password to use for ldap admin")
-        default_ldap_pwd: str = typer.prompt("Ldap admin password", hide_input=True)
+        typer.echo(_("Please input the password to use for ldap admin"))
+        default_ldap_pwd: str = typer.prompt(_("Ldap admin password"), hide_input=True)
 
         if default_ldap_pwd != "":
             replaceInFile("ldap.env", r"^\s*LDAP_ADMIN_PASSWORD=.*$", "LDAP_ADMIN_PASSWORD={}".format(default_ldap_pwd))
-            typer.echo(f"Password set to: {default_ldap_pwd}")
+            typer.echo(_("Password set to: {default_ldap_pwd}").format(default_ldap_pwd=default_ldap_pwd))
 
-    typer.echo("Would you like to enforce HTTPS? We recommend yes.")
+    typer.echo(_("Would you like to enforce HTTPS? We recommend yes."))
     enforce_https = is_enforce_https()
+
 
     if not enforce_https:
         for i in range(1):
-            typer.echo("Would you like to run an INSECURE and DANGEROUS server that will share your users's information if exposed to the Internet?")
-            insecure = typer.confirm("run insecure?")
+            typer.echo(_("Would you like to run an INSECURE and DANGEROUS server that will share your users's information if exposed to the Internet?"))
+            insecure = typer.confirm(_("run insecure?"))
             if insecure:
                 break
             if i==0:
-                raise RuntimeError("HTTPS is currently required to run a secure public server. Please restart and select to enforce HTTPS")
+                raise RuntimeError(_("HTTPS is currently required to run a secure public server. Please restart and select to enforce HTTPS"))
 
     save_progress('enforce_https', enforce_https)
 
-    typer.echo(f"Enforcing https: {enforce_https}")
+    typer.echo(_("Enforcing https: {enforce_https}").format(enforce_https=enforce_https))
     if enforce_https:
-        typer.echo("Please provide an admin email for security updates with HTTPS registration")
-        input_email: str = typer.prompt(f"admin email [({env['HTTPS_ADMIN_EMAIL']})]" , default=env['HTTPS_ADMIN_EMAIL'], show_default=False)
+        typer.echo(_("Please provide an admin email for security updates with HTTPS registration"))
+        input_email: str = typer.prompt(_("admin email [({envParam_https_admin_email})]").format(envParam_https_admin_email=envParam['HTTPS_ADMIN_EMAIL']), default=envParam['HTTPS_ADMIN_EMAIL'], show_default=False)
 
         if input_email != "":
-            env["HTTPS_ADMIN_EMAIL"] = input_email
+            envParam["HTTPS_ADMIN_EMAIL"] = input_email
             save_progress('env', {'HTTPS_DOMAIN': input_domain, 'HTTPS_ADMIN_EMAIL': input_email})
 
-        typer.echo("The system will now attempt to setup an HTTPS certificate for this server.")
-        typer.echo("For this to work you must have already have purchased/acquired a domain name (or subdomain) and setup a DNS A or AAAA record to point at this server's IP address.")
-        typer.echo("If you have not done this yet, please do it now...")
+        typer.echo(_("The system will now attempt to setup an HTTPS certificate for this server."))
+        typer.echo(_("For this to work you must have already have purchased/acquired a domain name (or subdomain) and setup a DNS A or AAAA record to point at this server's IP address."))
+        typer.echo(_("If you have not done this yet, please do it now..."))
         time.sleep(1)
-        proceed = typer.confirm("Domain is ready to proceed with certificate acquisition?", default=True)
+        proceed = typer.confirm(_("Domain is ready to proceed with certificate acquisition?"), default=True)
         if not proceed:
-            typer.echo("Re-run this script once the domain is ready!")
+            typer.echo(_("Re-run this script once the domain is ready!"))
             raise typer.Exit()
 
-        manual_certificate: str = typer.prompt("Do you wish to supply your own SSL certificate? If not, the script will use certbot (please make sure it is already installed).", default=False)
+        manual_certificate: str = typer.prompt(_("Do you wish to supply your own SSL certificate? If not, the script will use certbot (please make sure it is already installed)."), default=False)
 
         if not manual_certificate:
-            setup_certbot_certificate(env)
+            setup_certbot_certificate(envParam)
         else:
-            setup_manual_certificate(env)
+            setup_manual_certificate(envParam)
         
         save_progress('manual_certificate', manual_certificate)
-        typer.echo("Attempting to save updated https configuration")
+        typer.echo(_("Attempting to save updated https configuration"))
     return enforce_https
 
 def is_enforce_https() -> bool:
-    return typer.confirm("enforce https?", default=True)
+    return typer.confirm(_("enforce https?"), default=True)
 
 def is_cache_present() -> bool:
     return os.path.exists('progress.json') and os.stat('progress.json').st_size != 0 
@@ -161,24 +185,24 @@ def replaceInFile(file_path: str , pattern: str, subst: str):
 def setup_manual_certificate(env: Dict[str, str]):
     cert_fullchain_path: str = env['CERT_FULLCHAIN_PATH']
     cert_privkey_path: str = env['CERT_PRIVKEY_PATH']
-    typer.echo('Please enter path to fullchain .pem/.crt file')
-    cert_fullchain_path = typer.prompt(f"fullchain file [({cert_fullchain_path})]", default=cert_fullchain_path, show_default=False)
-    typer.echo('Please enter path to private key .pem file')
-    cert_privkey_path = typer.prompt(f"private key file [({cert_privkey_path})]", default=cert_privkey_path, show_default=False)
+    typer.echo(_('Please enter path to fullchain .pem/.crt file'))
+    cert_fullchain_path = typer.prompt(_("fullchain file [({cert_fullchain_path})]").format(cert_fullchain_path=cert_fullchain_path), default=cert_fullchain_path, show_default=False)
+    typer.echo(_('Please enter path to private key .pem file'))
+    cert_privkey_path = typer.prompt(_("private key file [({cert_privkey_path})]").format(cert_fullchain_path=cert_fullchain_path), default=cert_privkey_path, show_default=False)
     if not cert_fullchain_path or not cert_privkey_path:
-        typer.echo('Input not provided, re-run this script with correct inputs.')
+        typer.echo(_('Input not provided, re-run this script with correct inputs.'))
         raise typer.Exit()
     # Compute absolute paths from relative path inputs
     cert_fullchain_path = path.abspath(cert_fullchain_path)
     cert_privkey_path = path.abspath(cert_privkey_path)
     if not path.exists(cert_fullchain_path) or not path.exists(cert_privkey_path):
-        typer.echo('File at the given path do not exists, re-run this script with correct inputs.')
+        typer.echo(_('File at the given path do not exists, re-run this script with correct inputs.'))
         raise typer.Exit()
     env['CERT_FULLCHAIN_PATH'] = cert_fullchain_path
     env['CERT_PRIVKEY_PATH'] = cert_privkey_path
 
 def setup_certbot_certificate(env: Dict[str, str]):
-    typer.echo("Please enter your system Password")
+    typer.echo(_("Please enter your system Password"))
     os.system("sudo certbot certonly --standalone \
     --email {} \
     -d {} \
@@ -235,6 +259,8 @@ def deploy_stack(use_https: bool, env: Dict[str, str]):
 
 @app.callback(invoke_without_command=True)
 def install():
+    language_code = setup_locale() 
+    setup_translator(language_code)
     https, env = run_interactive_config()
     run_docker_builds()
     run_sync_endpoint_build()
