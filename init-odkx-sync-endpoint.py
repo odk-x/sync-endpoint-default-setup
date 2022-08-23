27adc82ef345b93290ea220770d86a7afd2e4c18
jesse5555 <natukunda55455@gmail.com>
2022-08-23 08:09:36 +0300

    resolved conflict

--- init-odkx-sync-endpoint.py
+++ init-odkx-sync-endpoint.py
@@ -92,7 +92,7 @@ def run_cache_setup(envParam: Dict[str, str]) -> bool:
     save_progress('env', {'HTTPS_DOMAIN': input_domain})
     typer.echo("")
 
-    use_custom_password = typer.confirm("_Do you want to use a custom LDAP administration password?")
+    use_custom_password = typer.confirm(_("Do you want to use a custom LDAP administration password?"))
 
     if use_custom_password:
         typer.echo("")
@@ -154,15 +154,15 @@ def check_valid_email(email):
     pattern = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
 
     if not (email!="" and re.match(pattern, email)):
-        typer.echo(f"Invalid email address: {email}")
-        typer.echo('Re-run this script with the correct email address.')
+        typer.echo(_("Invalid email address: {email}").format(email=email))
+        typer.echo(_("Re-run this script with the correct email address."))
         raise typer.Exit()
 
 def check_valid_domain(domain):
     pattern = r"^([A-Za-z0-9]\.|[A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9]\.){1,3}[A-Za-z]{2,6}$"
     if not (domain!="" and re.match(pattern, domain)):
-        typer.echo(f"Invalid domain: {domain}")
-        typer.echo('Re-run this script with the correct domain.')
+        typer.echo(_("Invalid domain: {domain}").format(domain=domain))
+        typer.echo(_("Re-run this script with the correct domain."))
         raise typer.Exit()
 
 def is_cache_present() -> bool:
@@ -218,7 +218,7 @@ def setup_manual_certificate(env: Dict[str, str]):
     env['CERT_PRIVKEY_PATH'] = cert_privkey_path
 
 def setup_certbot_certificate(env: Dict[str, str]):
-    typer.echo("Please enter your system Password")
+    typer.echo(_("Please enter your system Password"))
     try:
         subprocess.run("sudo certbot certonly --standalone \
         --email {} \
@@ -229,7 +229,7 @@ def setup_certbot_certificate(env: Dict[str, str]):
         --keep-until-expiring \
         --non-interactive".format(env["HTTPS_ADMIN_EMAIL"], env["HTTPS_DOMAIN"]), shell=True, check=True)
     except subprocess.CalledProcessError:
-        typer.echo("Error setting up certbot certificate.")
+        typer.echo(_("Error setting up certbot certificate."))
         typer.echo("")
 
 def write_to_env_file(filepath: str, env: Dict[str, str]):
@@ -261,7 +261,7 @@ def run_docker_builds():
         subprocess.run("docker build --pull -t odk/openldap openldap", shell=True, check=True)
         subprocess.run("docker build --pull -t odk/phpldapadmin phpldapadmin", shell=True, check=True)
     except subprocess.CalledProcessError:
-        typer.echo("Error pulling required docker images.")
+        typer.echo(_("Error pulling required docker images."))
         typer.echo("")
 
 def run_sync_endpoint_build():
@@ -270,7 +270,7 @@ def run_sync_endpoint_build():
                 cd sync-endpoint ; \
                 mvn -pl org.opendatakit:sync-endpoint-war,org.opendatakit:sync-endpoint-docker-swarm,org.opendatakit:sync-endpoint-common-dependencies clean install -DskipTests", shell=True, check=True)
     except subprocess.CalledProcessError:
-        typer.echo("Error building sync endpoint.")
+        typer.echo(_("Error building sync endpoint."))
         typer.echo("")
 
 def deploy_stack(use_https: bool, env: Dict[str, str]):
@@ -285,7 +285,7 @@ def deploy_stack(use_https: bool, env: Dict[str, str]):
         else:
             subprocess.run("docker stack deploy -c docker-compose.yml syncldap", shell=True, check=True)
     except subprocess.CalledProcessError:
-        typer.echo("Error deploying stack.")
+        typer.echo(_("Error deploying stack."))
         typer.echo("")
 
 @app.callback(invoke_without_command=True)
